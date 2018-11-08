6fadb583daf7199d2dda59c97b07ba233a5131c5
IvanSavchuk <isavchuk99@gmail.com>
2018-11-08 09:56:01 +0200

    add Admin panel

--- Gemfile
+++ Gemfile
@@ -2,8 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
-
+#add this gems
 gem 'devise'
+gem 'rails_admin', '~> 1.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  #add this code
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
