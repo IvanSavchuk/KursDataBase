6558d22ec8be6c099ef3c5949d1ab7125e9e3ca6
IvanSavchuk <isavchuk99@gmail.com>
2018-11-14 11:02:30 +0200

    updaate Post code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,12 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+  @posts = Post.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 
   # GET /posts/1
