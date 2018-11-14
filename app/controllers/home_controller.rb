66c547251f4e75f3fcfc695cf7da598338ef7cdf
IvanSavchuk <isavchuk99@gmail.com>
2018-11-14 10:21:42 +0200

    update code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
   def index
-    #add this code
-    if current_user
-      redirect_to posts_path
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   #add this code
-  root 'home#index'
+  root :to => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
