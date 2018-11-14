cb045ca676dea057cc2d6247686699bdfd74719b
IvanSavchuk <isavchuk99@gmail.com>
2018-11-14 12:21:52 +0200

    update and testing Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,29 +15,46 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    def show
+      @post = Post.find(params[:id])
+
+      respond_to do |format|
+        format.html  # show.html.erb
+        format.json  { render :json => @post }
+      end
+    end
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+  @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.html  { redirect_to(@post,
+                      :notice => 'Post was successfully created.') }
+        format.json  { render :json => @post,
+                      :status => :created, :location => @post }
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html  { render :action => "new" }
+        format.json  { render :json => @post.errors,
+                      :status => :unprocessable_entity }
       end
     end
   end
@@ -45,13 +62,17 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+  @post = Post.find(params[:id])
+
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+      if @post.update_attributes(params[:post])
+        format.html  { redirect_to(@post,
+                      :notice => 'Post was successfully updated.') }
+        format.json  { head :no_content }
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @post.errors,
+                      :status => :unprocessable_entity }
       end
     end
   end
